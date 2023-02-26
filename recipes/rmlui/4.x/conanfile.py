56e62dd7614d3bc9c90d85de3411ac2323f1a1a7
ZombieRaccoon <ZombieRaccoon@users.noreply.github.com>
2023-02-25 21:50:13 -0500

    Modernize RmlUi for Conan 2.0

--- recipes/rmlui/4.x/conanfile.py
+++ recipes/rmlui/4.x/conanfile.py
@@ -1,6 +1,9 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-import os
+import os, re
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, replace_in_file
+from conan.errors import ConanInvalidConfiguration
 
 
 class RmluiConan(ConanFile):
@@ -29,9 +32,8 @@ class RmluiConan(ConanFile):
         "with_lua_bindings": False,
         "with_thirdparty_containers": True
     }
-    build_requires = ["cmake/3.23.2"]
     exports_sources = ["CMakeLists.txt"]
-    generators = ["cmake", "cmake_find_package"]
+    generators = "CMakeDeps"
 
     @property
     def _minimum_compilers_version(self):
@@ -55,12 +57,12 @@ class RmluiConan(ConanFile):
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
-        
+            check_min_cppstd(self, self._minimum_cpp_standard)
+
         def lazy_lt_semver(v1, v2):
             lv1 = [int(v) for v in v1.split(".")]
             lv2 = [int(v) for v in v2.split(".")]
@@ -70,45 +72,46 @@ class RmluiConan(ConanFile):
         min_version = self._minimum_compilers_version.get(
             str(self.settings.compiler))
         if not min_version:
-            self.output.warn("{} recipe lacks information about the {} compiler support.".format(
-                self.name, self.settings.compiler))
+            self.output.warning(
+                "{self.name} recipe lacks information about the {self.settings.compiler} compiler support.")
         else:
             if lazy_lt_semver(str(self.settings.compiler.version), min_version):
-                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
-                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+                raise ConanInvalidConfiguration(
+                    "{self.name} requires C++{self._minimum_cpp_standard} support. The current compiler {self.settings.compiler} {self.settings.compiler.version} does not support it.")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_LUA_BINDINGS"] = self.options.with_lua_bindings
+        tc.variables["BUILD_SAMPLES"] = False
+        tc.variables["CUSTOM_CONFIGURATION"] = True
+        tc.variables["CUSTOM_INCLUDE_DIRS"] = ";".join([re.escape(dir) for dir in self.dependencies["robin-hood-hashing"].cpp_info.includedirs])
+        tc.variables["DISABLE_RTTI_AND_EXCEPTIONS"] = not self.options.enable_rtti_and_exceptions
+        tc.variables["ENABLE_PRECOMPILED_HEADERS"] = True
+        tc.variables["ENABLE_TRACY_PROFILING"] = False
+        tc.variables["MATRIX_ROW_MAJOR"] = self.options.matrix_mode == "row_major"
+        tc.variables["NO_FONT_INTERFACE_DEFAULT"] = self.options.font_interface is None
+        tc.variables["NO_THIRDPARTY_CONTAINERS"] = not self.options.with_thirdparty_containers
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def requirements(self):
         if self.options.font_interface == "freetype":
-            self.requires("freetype/2.10.4")
+            self.requires("freetype/2.13.0")
 
         if self.options.with_lua_bindings:
-            self.requires("lua/5.3.5")
+            self.requires("lua/5.4.4")
 
         if self.options.with_thirdparty_containers:
-            self.requires("robin-hood-hashing/3.11.3")
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+            self.requires("robin-hood-hashing/3.11.5")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def _configure_cmake(self):
         if not hasattr(self, "_cmake"):
             self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_LUA_BINDINGS"] = self.options.with_lua_bindings
-            self._cmake.definitions["BUILD_SAMPLES"] = False
-            self._cmake.definitions["CUSTOM_CONFIGURATION"] = True
-            self._cmake.definitions["CUSTOM_INCLUDE_DIRS"] = ";".join(self.deps_cpp_info["robin-hood-hashing"].include_paths)
-            self._cmake.definitions["DISABLE_RTTI_AND_EXCEPTIONS"] = not self.options.enable_rtti_and_exceptions
-            self._cmake.definitions["ENABLE_PRECOMPILED_HEADERS"] = True
-            self._cmake.definitions["ENABLE_TRACY_PROFILING"] = False
-            self._cmake.definitions["MATRIX_ROW_MAJOR"] = self.options.matrix_mode == "row_major"
-            self._cmake.definitions["NO_FONT_INTERFACE_DEFAULT"] = self.options.font_interface is None
-            self._cmake.definitions["NO_THIRDPARTY_CONTAINERS"] = not self.options.with_thirdparty_containers
-
             self._cmake.configure()
 
         return self._cmake
@@ -132,16 +135,13 @@ class RmluiConan(ConanFile):
             "if(PkgHelpers_AVAILABLE)": "if(FALSE)"
         }
 
-        cmakelists_path = os.path.join(
-            self._source_subfolder, "CMakeLists.txt")
+        cmakelists_path = os.path.join(self.source_folder, "CMakeLists.txt")
         for key, value in replace_mapping.items():
-            tools.replace_in_file(cmakelists_path, key, value, strict=False)
+            replace_in_file(self, cmakelists_path, key, value, strict=False)
 
         if self.options.with_thirdparty_containers:
-            config_path = os.path.join(self._source_subfolder,
-                                       "Include", "RmlUi", "Config", "Config.h")
-            tools.replace_in_file(
-                config_path, "\"../Core/Containers/robin_hood.h\"", "<robin_hood.h>")
+            config_path = os.path.join(self.source_folder, "Include", "RmlUi", "Config", "Config.h")
+            replace_in_file(self, config_path, "\"../Core/Containers/robin_hood.h\"", "<robin_hood.h>")
 
     def build(self):
         self._patch_sources()
@@ -149,7 +149,8 @@ class RmluiConan(ConanFile):
 
     def package(self):
         self._configure_cmake().install()
-        self.copy("*LICENSE.txt", dst="licenses", src=self._source_subfolder, excludes=("Samples/*", "Tests/*"))
+        copy(self, "*LICENSE.txt", self.source_folder,
+             os.path.join(self.package_folder, "licenses"), excludes=("Samples/*", "Tests/*"))
 
     def package_info(self):
         if self.options.matrix_mode == "row_major":
